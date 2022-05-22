1ae54f6723e43ce05773f93e6e885999bc110ae0
Sameer Bagwan <sameerbagwan132@gmail.com>
2022-05-22 17:46:49 +0530

    Updated 7th Question

--- Question7/app/src/main/java/com/example/question7/MainActivity.java
+++ Question7/app/src/main/java/com/example/question7/MainActivity.java
@@ -1,14 +1,211 @@
 package com.example.question7;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView playerPosition,playerDuration;
+    SeekBar seekBar;
+    ImageView forward_btn, rewind_btn, play_btn, pause_btn;
+
+    MediaPlayer mediaPlayer;
+    Handler handler = new Handler();
+    Runnable runnable;
+
+    ListView songListView;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> audioList;
+    Uri SongName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerPosition = findViewById(R.id.playerPosition);
+        playerDuration = findViewById(R.id.playDuration);
+        seekBar = findViewById(R.id.seekbar);
+        forward_btn = findViewById(R.id.forward);
+        rewind_btn = findViewById(R.id.rewind);
+        play_btn = findViewById(R.id.play);
+        pause_btn = findViewById(R.id.pause);
+
+
+        //songList code
+
+        songListView = (ListView) findViewById(R.id.listView);
+        audioList = new ArrayList<>();
+
+        String[] proj = { MediaStore.Audio.Media._ID,MediaStore.Audio.Media.DISPLAY_NAME };
+        Cursor audioCursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, proj, null, null, null);
+
+
+        if(audioCursor != null){
+            if(audioCursor.moveToFirst()){
+                do{
+                    int audioIndex = audioCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME);
+
+                    audioList.add(audioCursor.getString(audioIndex));
+                }while(audioCursor.moveToNext());
+            }
+        }
+        audioCursor.close();
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1, audioList){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView ListItemShow = (TextView) view.findViewById(android.R.id.text1);
+                ListItemShow.setTextColor(Color.parseColor("#ffffff"));
+                return view;
+            }
+        };
+        songListView.setAdapter(adapter);
+
+        songListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Uri MusicUri= android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                SongName = MusicUri;
+                mediaPlayer = MediaPlayer.create(getApplicationContext(),MusicUri);
+            }
+        });
+
+        //Player Code
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.tokyo_drift);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                handler.postDelayed(this,500);
+            }
+        };
+
+        int duration = mediaPlayer.getDuration();
+        String sDuration = convertFormat(duration);
+        playerDuration.setText(sDuration);
+
+        //OnClickListeners
+
+        play_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                play_btn.setVisibility(View.GONE);
+                pause_btn.setVisibility(View.VISIBLE);
+                mediaPlayer.start();
+                seekBar.setMax(mediaPlayer.getDuration());
+                handler.postDelayed(runnable,0);
+            }
+        });
+
+        pause_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pause_btn.setVisibility(View.GONE);
+                play_btn.setVisibility(View.VISIBLE);
+                mediaPlayer.pause();
+                handler.removeCallbacks(runnable);
+            }
+        });
+
+        forward_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int curr_position = mediaPlayer.getCurrentPosition();
+                int duration = mediaPlayer.getDuration();
+                if (mediaPlayer.isPlaying() && duration != curr_position){
+                    curr_position = curr_position + 5000;
+                    playerPosition.setText(convertFormat(curr_position));
+                    mediaPlayer.seekTo(curr_position);
+                }
+            }
+        });
+
+        rewind_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int curr_position = mediaPlayer.getCurrentPosition();
+                if (mediaPlayer.isPlaying() && curr_position > 5000){
+                    curr_position = curr_position - 5000;
+                    playerPosition.setText(convertFormat(curr_position));
+                    mediaPlayer.seekTo(curr_position);
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+                playerPosition.setText(convertFormat(mediaPlayer.getCurrentPosition()));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                pause_btn.setVisibility(View.GONE);
+                play_btn.setVisibility(View.VISIBLE);
+                mediaPlayer.seekTo(0);
+            }
+        });
+
+    }
+
+    //Methods
+
+    private String convertFormat(int duration) {
+        return String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(duration),
+                TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
+
     }
 }
