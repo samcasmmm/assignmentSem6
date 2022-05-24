dd6dcf5071bd8848a506cf5ab616ecd870093c6d
Sameer Bagwan <sameerbagwan132@gmail.com>
2022-05-24 12:01:58 +0530

    Updated

--- Question1/app/src/main/java/com/example/question1/MainActivity.java
+++ Question1/app/src/main/java/com/example/question1/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
---
+++ sample/app/src/main/java/com/example/sample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(MainActivity.this, "You click", Toast.LENGTH_SHORT).show();
+    }
+}
