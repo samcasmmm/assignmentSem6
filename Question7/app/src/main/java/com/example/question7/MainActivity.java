d39e4d3105aea16f448b973a580a21e64d9f8b32
Sameer Bagwan <sameerbagwan132@gmail.com>
2022-05-27 20:15:28 +0530

    Updated

--- Question7/app/src/main/java/com/example/question7/MainActivity.java
+++ Question7/app/src/main/java/com/example/question7/MainActivity.java
@@ -1,82 +1,30 @@
 package com.example.question7;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Color;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.MediaStore;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView playerPosition,playerDuration;
-    SeekBar seekBar;
-    ImageView forward_btn, rewind_btn, play_btn, pause_btn;
-
+    ImageView  play_btn, pause_btn;
     MediaPlayer mediaPlayer;
     Handler handler = new Handler();
     Runnable runnable;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playerPosition = findViewById(R.id.playerPosition);
-        playerDuration = findViewById(R.id.playDuration);
-        seekBar = findViewById(R.id.seekbar);
-        forward_btn = findViewById(R.id.forward);
-        rewind_btn = findViewById(R.id.rewind);
         play_btn = findViewById(R.id.play);
         pause_btn = findViewById(R.id.pause);
 
-
-        //Player Code
-
         mediaPlayer = MediaPlayer.create(this,R.raw.tokyo_drift);
-
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                handler.postDelayed(this,500);
-            }
-        };
-
-        int duration = mediaPlayer.getDuration();
-        String sDuration = convertFormat(duration);
-        playerDuration.setText(sDuration);
-
-        //OnClickListeners
 
         play_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 play_btn.setVisibility(View.GONE);
                 pause_btn.setVisibility(View.VISIBLE);
                 mediaPlayer.start();
-                seekBar.setMax(mediaPlayer.getDuration());
                 handler.postDelayed(runnable,0);
             }
         });
@@ -99,69 +46,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        forward_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int curr_position = mediaPlayer.getCurrentPosition();
-                int duration = mediaPlayer.getDuration();
-                if (mediaPlayer.isPlaying() && duration != curr_position){
-                    curr_position = curr_position + 5000;
-                    playerPosition.setText(convertFormat(curr_position));
-                    mediaPlayer.seekTo(curr_position);
-                }
-            }
-        });
-
-        rewind_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int curr_position = mediaPlayer.getCurrentPosition();
-                if (mediaPlayer.isPlaying() && curr_position > 5000){
-                    curr_position = curr_position - 5000;
-                    playerPosition.setText(convertFormat(curr_position));
-                    mediaPlayer.seekTo(curr_position);
-                }
-            }
-        });
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser){
-                    mediaPlayer.seekTo(progress);
-                }
-                playerPosition.setText(convertFormat(mediaPlayer.getCurrentPosition()));
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                pause_btn.setVisibility(View.GONE);
-                play_btn.setVisibility(View.VISIBLE);
-                mediaPlayer.seekTo(0);
-            }
-        });
-
     }
 
-    //Methods
-
-    private String convertFormat(int duration) {
-        return String.format("%02d:%02d",
-                TimeUnit.MILLISECONDS.toMinutes(duration),
-                TimeUnit.MILLISECONDS.toSeconds(duration) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
-
-    }
 }
--- sample/app/src/main/java/com/example/sample/MainActivity.java
+++ sample/app/src/main/java/com/example/sample/MainActivity.java
@@ -5,19 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    EditText et_Username, et_Passwd;
+    Button registerBtn;
+    RadioButton rb_Male, rb_Female;
+    CheckBox cb_Web,cb_Android;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void showToast(View view) {
-        Toast.makeText(MainActivity.this, "You click", Toast.LENGTH_SHORT).show();
+        et_Username = findViewById(R.id.et_Username);
+        et_Passwd = findViewById(R.id.et_Passwd);
+        registerBtn = findViewById(R.id.registerBtn);
+        rb_Male = findViewById(R.id.rb_Male);
+        rb_Female = findViewById(R.id.rb_Female);
+        cb_Web = findViewById(R.id.cb_Web);
+        cb_Android = findViewById(R.id.cb_Android);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = et_Username.getText().toString().trim();
+                String passwd = et_Passwd.getText().toString().trim();
+                String course = "";
+                String gender = "";
+
+                if (cb_Web.isChecked()){ course = "Web"; }
+
+                if (cb_Android.isChecked()){ course = "Android"; }
+
+                if (cb_Android.isChecked() && cb_Web.isChecked()){
+                    course = "Web + Android";
+                }
+
+                if (rb_Male.isChecked()){ gender = "Male"; }
+
+                if (rb_Female.isChecked()){ gender = "Female"; }
+
+                String toast_text = "Username : "+user+" Passwd : "+passwd+" Course : "+course+" Gender : "+gender;
+
+                Toast.makeText(MainActivity.this, toast_text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
